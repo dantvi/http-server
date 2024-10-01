bd74f768cf6f45d39596e9f48d0b3aed88f42b3c
dantvi <daniel.tving@gmail.com>
2024-10-01 17:18:53 +0200

    parameterized urls

--- index.js
+++ index.js
@@ -4,16 +4,47 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+    {
+        id: '0',
+        name: 'Nikola Tesla',
+    },
+    {
+        id: '1',
+        name: 'Sir Isaac Newton',
+    },
+    {
+        id: '2',
+        name: 'Albert Einstein',
+    },
+    {
+        id: '3',
+        name: 'Marie Curie',
+    },
+    {
+        id: '4',
+        name: 'Charles Darwin',
+    },
+    {
+        id: '5',
+        name: 'Rosalind Franklin',
+    },
+]
+
 server.on('request', (req, res) => {
-    if (req.url === '/friends') {
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-        res.end(JSON.stringify({
-            id: 1,
-            name: 'Sir Isaac Newton'
-        }));
-    } else if (req.url === '/messages') {
+    const items = req.url.split('/');
+    console.log('items', items);
+    // /friends/2 --> ['', 'friends', '2']
+    if (items[1] === 'friends') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
         // Rendering HTML in the browser with data from the server
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
